79921079662e56356365e39a5423aebd52eddb35
sumeyyegsu <smy_konak@hotmail.com>
2021-01-04 20:13:34 +0300

    update: instead of all training data, now, it reads only "ambiguous sentences" from training data.

--- workshopTask2.py
+++ workshopTask2.py
@@ -8,6 +8,16 @@
 #   Precision = number of correctly resolved anaphors divided by the total number of anaphors attempted to be resolved
 #   Recall = number of correctly resolved anaphors divided by the total number of unambiguous anaphors
 
+# Useful links:
+
+
+# Detection:
+#   Precision = number of sentences correctly detected as ambiguous divided by the total number detected by the system as ambiguous
+#   Recall = number of sentences correctly detected as ambiguous divided by the total number annotated by humans as ambiguous
+# Resolution:
+#   Precision = number of correctly resolved anaphors divided by the total number of anaphors attempted to be resolved
+#   Recall = number of correctly resolved anaphors divided by the total number of unambiguous anaphors
+
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
@@ -64,7 +74,22 @@ def featureExtraction(tags):
 
 
 def main():
+    training_sentences_y = []
+    training_sentences_y_id = []
+    # open file in read mode
+    with open('disambiguation_answers_file.csv', 'r', encoding='utf8') as read_obj:
+        # pass the file object to reader() to get the reader object
+        csv_reader = reader(read_obj)
+        # Iterate over each row in the csv using reader object
+        for row in csv_reader:
+            # row variable is a list that represents a row in csv
+            training_sentences_y.append(row[1])
+            training_sentences_y_id.append(row[0])
+    del training_sentences_y[0]  # delete header
+    del training_sentences_y_id[0]  # delete header
+
     training_sentences_x = []
+    training_sentences_x_id = []
     # open file in read mode
     with open('training_set.csv', 'r', encoding='utf8') as read_obj:
         # pass the file object to reader() to get the reader object
@@ -74,34 +99,21 @@ def main():
             # row variable is a list that represents a row in csv
             new_row = row[1].replace("<referential>", "")
             new_row = new_row.replace("</referential>", "")
-            training_sentences_x.append(new_row)
-
-    del training_sentences_x[0]  # delete header
-
-    training_sentences_y = []
-    # open file in read mode
-    with open('disambiguation_answers_file.csv', 'r', encoding='utf8') as read_obj:
-        # pass the file object to reader() to get the reader object
-        csv_reader = reader(read_obj)
-        # Iterate over each row in the csv using reader object
-        for row in csv_reader:
-            # row variable is a list that represents a row in csv
-            training_sentences_y.append(row)
+            if row[0] in training_sentences_y_id: # if it is ambiguous, add
+                training_sentences_x.append(new_row)
 
     feature_vectors = []
     for sentence in training_sentences_x:
         tags = preprocessing(sentence)
-        print(sentence)
         feature_vector = featureExtraction(tags)
         feature_vectors.append(feature_vector)
-        print(feature_vector)
-
+    '''
     lreg = linear_model.LogisticRegression()
     lreg.fit(feature_vectors, training_sentences_y)
 
     predicted_sentences_y = lreg.predict(feature_vectors)
     print_model_performace_for_test_doc(training_sentences_y, predicted_sentences_y)
-
+    '''
 
 if __name__ == '__main__':
     main()
