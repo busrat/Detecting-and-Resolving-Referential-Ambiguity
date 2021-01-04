571c22c8d87e6f3e82c804e0bb4bc720a18ae815
sumeyyegsu <smy_konak@hotmail.com>
2021-01-04 20:24:45 +0300

    antecedent_indexes and anaphora_indexes are added.

--- workshopTask2.py
+++ workshopTask2.py
@@ -8,16 +8,6 @@
 #   Precision = number of correctly resolved anaphors divided by the total number of anaphors attempted to be resolved
 #   Recall = number of correctly resolved anaphors divided by the total number of unambiguous anaphors
 
-# Useful links:
-
-
-# Detection:
-#   Precision = number of sentences correctly detected as ambiguous divided by the total number detected by the system as ambiguous
-#   Recall = number of sentences correctly detected as ambiguous divided by the total number annotated by humans as ambiguous
-# Resolution:
-#   Precision = number of correctly resolved anaphors divided by the total number of anaphors attempted to be resolved
-#   Recall = number of correctly resolved anaphors divided by the total number of unambiguous anaphors
-
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
@@ -56,12 +46,11 @@ def preprocessing(sentence):
     # 4. POS tagging
     tagged_words = nltk.pos_tag(lemmatized_words)
 
-    print(tagged_words)
     return tagged_words
 
 
 def featureExtraction(tags):
-
+    print(tags)
     feature_vector = [0]
 
     # for candidate i and a pronoun j
@@ -69,6 +58,19 @@ def featureExtraction(tags):
     # gender_agreement: i-j aynı cinsiyette mi (her - she)
     # number_agreement: i-j'nin ikisi de çoğul ya da tekil mi
     # parallelism: i-j ikisi de subject ya da object mi
+    
+    antecedent_indexes = [] # The man is hungry, (NN*)
+    anaphora_indexes = []   # so, he eats dinnner. (PRP*)
+    index = 0
+    for tag in tags:
+        if tag[1].startswith("NN"):
+            antecedent_indexes.append(index)
+
+        if tag[1] == "PRP":
+            anaphora_indexes.append(index)
+
+        if antecedent_indexes != [] and anaphora_indexes != []: # there should be at least one from both of them.
+            
 
     return feature_vector
 
