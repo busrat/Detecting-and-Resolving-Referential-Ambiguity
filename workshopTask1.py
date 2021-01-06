ee31cd09bb0a11d0aa49b2356735d6188a4368a6
ysmn32 <ysmn.akan3@gmail.com>
2021-01-07 01:05:12 +0300

    Update workshopTask1.py
    Several bugs fixed, comment added. 83/100

--- workshopTask1.py
+++ workshopTask1.py
@@ -62,7 +62,7 @@ def preprocessing(sentence):
 
 def featureExtraction(tags):
     rule1_prp_flag = False
-    feature_vector = 16 * [0]
+    feature_vector = 15 * [0]
     prp_counter = 0
     nn_counter1 = 0
     nn_counter2 = 0
@@ -86,12 +86,13 @@ def featureExtraction(tags):
             conj_counter += 1
 
         # RULE 2: 1 if there are more than one PRP
+        # RULE 7: The number of PRP(pronoun) in the sentence
         if tag[1] == "PRP":
             prp_counter += 1
             if prp_counter > 1 and feature_vector[2] == 0:
                 feature_vector[2] = 1
 
-        # RULE 3: noun + noun -> pronoun varsa 1 yoksa 0 - tekil
+        # RULE 3: 1 if there is NN(noun, singular or mass) or NNP (proper noun, singular) in the sentence
         if feature_vector[3] == 0:
             if tag[1] == "NN" or tag[1] == "NNP":
                 nn_counter1 += 1
@@ -99,51 +100,54 @@ def featureExtraction(tags):
                 if tag[0].lower() in ['he', 'she', 'it']:
                     feature_vector[3] = 1
 
-        # RULE 11: noun + noun -> pronoun varsa 1 yoksa 0 - çoğul
+        # RULE 13: 1 if there is NNS(noun, plural or mass) or NNPS (proper noun, plural) in the sentence
         if feature_vector[3] == 0:
             if tag[1] == "NNS" or tag[1] == "NNPS":
                 nn_counter2 += 1
             if nn_counter2 >= 2:
                 if tag[0].lower() in ['they']:
-                    feature_vector[14] = 1
+                    feature_vector[13] = 1
 
-        # RULE 4: verb sayısı
+        # RULE 4: 1 if the number of VB (verb) is greater than 4
+        # RULE 8: The number of VB (verb) in the sentence
         if tag[1].startswith("VB"):
             verb_counter += 1
             if verb_counter > 4:
                 if feature_vector[4] == 0:
                     feature_vector[4] = 1
 
-        # RULE 5: cümle değiştiren noktalama sayısı
+        # RULE 5: 1 if the number of punctuation marks that separate sentences is greater than 2 and
+        # RULE 9: The number of punctuation marks that separate sentences
         if tag[1] in [',', '.', ';', '!', '?', ':', '``', "''"]:
             punctuation_counter += 1
             if punctuation_counter > 2 and feature_vector[9] == 0:
                 feature_vector[5] = 1
 
-        # RULE 6: referent olabileceklerin sayısı
+        # RULE 6: The number of all nouns that can be referent
         if tag[1].startswith("NN"):
         #if tag[1] == "NN" or tag[1] == "NNS" or tag[1] == "NNP":
             noun_counter += 1
 
-        #1 if there is WRB (when, where etc.)
+        # Control flag for RULE 11: True if there is WRB (when, where etc.)
         if tag[1] == "WRB":
             wrb_flag = True
 
-        # RULE: 1 if there are more than one capital letter
-        if feature_vector[12] == 0:
+        # RULE 11: 1 if there are more than one capital letter
+        if feature_vector[11] == 0:
             uppercaseLetters += len(re.findall(r'[A-Z]', tag[0]))
             if uppercaseLetters > 1:
-                feature_vector[12] = 1
-        #RULE: 1 if the number of IN (with, in, under, of etc) greater than 2
+                feature_vector[11] = 1
+
+        # RULE 12: 1 if the number of IN (with, in, under, of etc) greater than 2greater than 2
         if tag[1] == "IN":
             in_counter += 1
-            if in_counter > 2 and feature_vector[13] == 0:
-                feature_vector[13] = 1
+            if in_counter > 2 and feature_vector[12] == 0:
+                feature_vector[12] = 1
 
-        # RULE: belirsizlik yaratan kelimeler var mı? bazı işler: hangi işler? bu okul:hangi okul? vs
+        # RULE 14: 1 if there are words that cause ambiguity such as some jobs, this school etc.
         if tag[0].lower() in ['more', 'some', 'any', 'other', 'most', 'another', 'this', 'that', 'many', 'certain'] and \
-                feature_vector[15] == 0:
-            feature_vector[15] = 1
+                feature_vector[14] == 0:
+            feature_vector[14] = 1
 
     feature_vector[7] = prp_counter
     feature_vector[6] = noun_counter
@@ -151,8 +155,9 @@ def featureExtraction(tags):
     feature_vector[9] = punctuation_counter
     feature_vector[1] = conj_counter
 
+    # RULE 10: 1 if there is CC and PRP in the sentence
     if wrb_flag == True and prp_counter > 0:
-        feature_vector[11] = 1
+        feature_vector[10] = 1
 
     return feature_vector
 
@@ -160,13 +165,14 @@ def featureExtraction(tags):
 def main():
     training_sentences_x = []
     # open file in read mode
-    with open('training_set.csv', 'r', encoding='utf8') as read_obj:
+    with open('Data/training_set.csv', 'r', encoding='utf8') as read_obj:
         # pass the file object to reader() to get the reader object
         csv_reader = reader(read_obj)
         # Iterate over each row in the csv using reader object
         for row in csv_reader:
-            sentence = ', '.join(row[1:]) # csv_reader , lerden ayırdığı için cümle için virgülleri de ayırıyor. onları birleştiriyoruz.
+            sentence = ', '.join(row[1:]) # To read the sentences separated by commas to the end
 
+            # Delete statements indicating referentials from sentences
             try: sentence = sentence.replace('<referential>', "")
             except: pass
             try: sentence = sentence.replace('<referential id="a">', "")
@@ -182,13 +188,14 @@ def main():
     training_sentences_y = []
     # open file in read mode
     i = 0
-    with open('detection_answers_file.csv', 'r', encoding='utf8') as read_obj:
+    with open('Data/detection_answers_file.csv', 'r', encoding='utf8') as read_obj:
         # pass the file object to reader() to get the reader object
         csv_reader = reader(read_obj)
         # Iterate over each row in the csv using reader object
         for row in csv_reader:
             # row variable is a list that represents a row in csv
             if not i == 0:
+                # Create label vector
                 if row[1] == "AMBIGUOUS":
                     training_sentences_y.append(1)
                 else:
